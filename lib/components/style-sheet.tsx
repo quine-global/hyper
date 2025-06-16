06f040a5663d7f1f983ebce2e6e73ecf25f26444
Philip Peterson <1326208+philip-peterson@users.noreply.github.com>
2025-06-15 17:22:21 -0700

    Fix scrollbar width being too small on Retina Mac (#167)

--- lib/components/style-sheet.tsx
+++ lib/components/style-sheet.tsx
@@ -1,14 +1,17 @@
 import React, {forwardRef} from 'react';
 
 import type {StyleSheetProps} from '../../typings/hyper';
+import { useDevicePixelRatio } from 'use-device-pixel-ratio';
 
 const StyleSheet = forwardRef<HTMLStyleElement, StyleSheetProps>((props, ref) => {
   const {borderColor} = props;
 
+  const dpr = useDevicePixelRatio();
+
   return (
     <style jsx global ref={ref}>{`
       ::-webkit-scrollbar {
-        width: 5px;
+        width: ${5 * dpr}px;
       }
       ::-webkit-scrollbar-thumb {
         -webkit-border-radius: 10px;
