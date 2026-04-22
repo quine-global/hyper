051c8cc5e81c58044f4657f4aa94758f854b8cfa
Philip Peterson <1326208+philip-peterson@users.noreply.github.com>
2026-04-22 05:29:29 -0400

    Delete ava for simplicity

--- ava-e2e.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  files: ['test/*'],
-  extensions: ['ts'],
-  require: ['ts-node/register/transpile-only'],
-  timeout: '2m',
-  verbose: true,
-  // Due to permissions issues, Windows needs cache turned off
-  cache: false
-};
--- ava.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  files: ['test/unit/*'],
-  extensions: ['ts'],
-  require: ['ts-node/register/transpile-only'],
-  verbose: true,
-  // Due to permissions issues, Windows needs cache turned off
-  cache: false
-};
